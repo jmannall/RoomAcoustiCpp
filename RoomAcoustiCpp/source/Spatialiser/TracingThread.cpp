51e0b8c84ba7ffe29cc083c29fca62a93640c524
Matteo-Scerbo <matteo.scerbo@hotmail.it>
2025-08-07 18:36:54 +0100

    Revised pseudocode.

--- RoomAcoustiCpp/source/Spatialiser/TracingThread.cpp
+++ RoomAcoustiCpp/source/Spatialiser/TracingThread.cpp
@@ -10,9 +10,11 @@ namespace RAC
 			shared_ptr<Reverb> sharedReverb = mReverb.lock();
 			sharedReverb->GetReverbSourceDirections(reverbDirections);
 
-			// TODO: Set the pointer TriangleSoA& triangles
-			// TODO: Somehow initialize std::vector<std::vector<int>> pathIndexing (passed as argument to this constructor?)
-			// TODO: Initialize RayPencil hemispherePencil (Is the number of rays passed by Context? A global constant (or variable)? A compiler flag?
+			// TODO: Initialize TriangleSoA& triangles; pointing towards the (existing) attribute of mRoom
+			
+			// TODO: Somehow initialize std::vector<std::vector<int>> pathIndexing (passed by Context as an argument to this constructor?)
+			
+			// TODO: Initialize RayPencil hemispherePencil; Is the number of rays passed by Context as an argument to this constructor? A global constant (or variable)? A compiler flag?
 		}
 
 		void TracingThread::RunTracing()
@@ -32,13 +34,15 @@ namespace RAC
 				hemispherePencil.move_origin(mListenerPosition);
 				hemispherePencil.trace_all(triangles);
 
-				// TODO: Compute energy contributions per propagation path, per reverbDirection
+				// TODO: Compute energy contributions of each reverbDirection to each propagation path (using pathIndexing in combination with hemispherePencil.get_indices)
 				
-				// TODO: For each slope in mReverb,
-					// TODO: Get reference to right eigenvector
-					// TODO: For each reverbDirection,
-						// TODO: residue = dot(energy contributions, eigenvector)
-					// TODO: Return all directions' residues to the FDN
+				// TODO: for (slope_idx = 0; slope_idx < mReverb.get_num_slopes(); ++slope_idx)
+					// TODO: eigenvector = mReverb.get_right_eigenvector(slope_idx);
+					// TODO: residues = zeros(reverbDirections.size());
+					// TODO: for (dir_idx = 0; dir_idx < reverbDirections.size(); ++dir_idx)
+						// TODO: residues[dir_idx] = dot(energy_contributions[dir_idx], eigenvector)
+					// TODO: Return residues to FDN[slope_idx]
+					// TODO: mReverb->UpdateListenerResidues(slope_idx, residues);
 			}
 
 			shared_ptr<SourceManager> sharedSource = mSourceManager.lock();
@@ -50,11 +54,12 @@ namespace RAC
 					hemispherePencil.move_origin(source.position);
 					hemispherePencil.trace_all(triangles);
 
-					// TODO: Compute energy contributions per propagation path
+					// TODO: Compute energy contributions to each propagation path (using pathIndexing in combination with hemispherePencil.get_indices)
 
-					// TODO: For each slope in mReverb,
-						// TODO: Get reference to left eigenvector
-						// TODO: residue = dot(energy contributions, eigenvector)
+					// TODO: residues = zeros(mReverb.get_num_slopes());
+					// TODO: for (slope_idx = 0; slope_idx < mReverb.get_num_slopes(); ++slope_idx)
+						// TODO: eigenvector = mReverb.get_left_eigenvector(slope_idx);
+						// TODO: residues[slope_idx] = dot(energy contributions, eigenvector)
 					// TODO: sharedSource->UpdateSourceResidues(source.id, residues);
 				}
 			}
