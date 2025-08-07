7feefdefca75f1d5c245bbc78718e53cdf4b3e37
Matteo-Scerbo <matteo.scerbo@hotmail.it>
2025-08-07 18:25:41 +0100

    Placeholder TracingThread.

--- RoomAcoustiCpp/include/Spatialiser/TracingThread.h
+++ RoomAcoustiCpp/include/Spatialiser/TracingThread.h
@@ -5,14 +5,66 @@
 #ifndef Tracing_Thread_h
 #define Tracing_Thread_h
 
+// Common headers
+#include "Common/Vec3.h" 
+
+// Spatialiser headers
+#include "Spatialiser/Types.h"
 #include "Spatialiser/Room.h"
 #include "Spatialiser/Reverb.h"
+#include "Spatialiser/SourceManager.h"
 #include "Spatialiser/TracingUtils.h" // Indirectly includes TracingTypes
 
 namespace RAC
 {
+	using namespace Common;
 	namespace Spatialiser
 	{
+		/**
+		* @brief Class that runs the ray tracing
+		*/
+		class TracingThread
+		{
+		public:
+			/**
+			* @brief Constructor that initialises the TracingThread class
+			*
+			* @param room Pointer to the room class
+			* @param sourceManager Pointer to the source manager class
+			* @param reverb Pointer to the late reverb class
+			* @param frequencyBands Frequency bands for graphic equalisers
+			*/
+			TracingThread(shared_ptr<Room> room, shared_ptr<SourceManager> sourceManager, shared_ptr<Reverb> reverb);
+
+			/**
+			* @brief Updates the stored listener position
+			*/
+			inline void SetListenerPosition(const Vec3& position) { lock_guard<std::mutex> lock(dataStoreMutex); mListenerPositionStore = position; }
+			
+			/**
+			* @brief Process the ray-tracing and update...
+			*/ // TODO: Update what?
+			void RunTracing();
+
+		private:
+			weak_ptr<Room> mRoom;							// Pointer to the room class
+			weak_ptr<SourceManager> mSourceManager;			// Pointer to the source manager class
+			weak_ptr<Reverb> mReverb;						// Pointer to the late reverb class
+
+			TriangleMeshSoA &triangles;
+
+			RayPencil hemispherePencil;
+
+			std::vector<std::vector<int>> pathIndexing;
+
+			std::vector<Vec3> reverbDirections;
+
+			std::vector<Source::Data> mSources;		// Store sources
+
+			Vec3 mListenerPosition;					// The listener position (can be accessed freely)
+			Vec3 mListenerPositionStore;			// Stores the listener position (Mutex must be locked to access)
+			std::mutex dataStoreMutex;				// Protects mListenerPositionStore
+		};
 	}
 }
 #endif
--- RoomAcoustiCpp/source/Spatialiser/TracingThread.cpp
+++ RoomAcoustiCpp/source/Spatialiser/TracingThread.cpp
@@ -1,1 +1,63 @@
 #include "Spatialiser/TracingThread.h"
+namespace RAC
+{
+	using namespace Common;
+	namespace Spatialiser
+	{
+		TracingThread::TracingThread(shared_ptr<Room> room, shared_ptr<SourceManager> sourceManager, shared_ptr<Reverb> reverb) :
+			mRoom(room), mSourceManager(sourceManager), mReverb(reverb) // TODO: Am I doing this right?
+		{
+			shared_ptr<Reverb> sharedReverb = mReverb.lock();
+			sharedReverb->GetReverbSourceDirections(reverbDirections);
+
+			// TODO: Set the pointer TriangleSoA& triangles
+			// TODO: Somehow initialize std::vector<std::vector<int>> pathIndexing (passed as argument to this constructor?)
+			// TODO: Initialize RayPencil hemispherePencil (Is the number of rays passed by Context? A global constant (or variable)? A compiler flag?
+		}
+
+		void TracingThread::RunTracing()
+		{
+			// TODO: PROFILE_TracingThread
+
+			bool listenerMoved = false;
+			{
+				lock_guard<std::mutex> lock(dataStoreMutex);
+				if (mListenerPosition != mListenerPositionStore)
+				{
+					mListenerPosition = mListenerPositionStore;
+					listenerMoved = true;
+				}
+			}
+			if (listenerMoved) {
+				hemispherePencil.move_origin(mListenerPosition);
+				hemispherePencil.trace_all(triangles);
+
+				// TODO: Compute energy contributions per propagation path, per reverbDirection
+				
+				// TODO: For each slope in mReverb,
+					// TODO: Get reference to right eigenvector
+					// TODO: For each reverbDirection,
+						// TODO: residue = dot(energy contributions, eigenvector)
+					// TODO: Return all directions' residues to the FDN
+			}
+
+			shared_ptr<SourceManager> sharedSource = mSourceManager.lock();
+			sharedSource->ResetUnusedSources();
+			mSources = sharedSource->GetSourceData();
+
+			for (Source::Data& source : mSources) {
+				if (source.hasChanged) {
+					hemispherePencil.move_origin(source.position);
+					hemispherePencil.trace_all(triangles);
+
+					// TODO: Compute energy contributions per propagation path
+
+					// TODO: For each slope in mReverb,
+						// TODO: Get reference to left eigenvector
+						// TODO: residue = dot(energy contributions, eigenvector)
+					// TODO: sharedSource->UpdateSourceResidues(source.id, residues);
+				}
+			}
+		}
+	}
+}
