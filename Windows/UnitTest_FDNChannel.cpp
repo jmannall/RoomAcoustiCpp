7a3b3bf691451ceeae27b80237a57a4e9d463ff9
jmannall <110119245+jmannall@users.noreply.github.com>
2025-09-04 11:01:21 +0100

    Fix num coefficients in UnitTests to match default Config

--- Windows/UnitTest_FDNChannel.cpp
+++ Windows/UnitTest_FDNChannel.cpp
@@ -21,7 +21,7 @@ namespace RAC
 		TEST_METHOD(DelayLine)
 		{
 			const int delay = 127;
-			const Coefficients T60({ 0.1, 0.2, 0.3, 0.4 });
+			const Coefficients T60({ 0.1, 0.2, 0.3, 0.4, 0.4 });
 			const std::shared_ptr<Config> config = std::make_shared<Config>();
 			const Real lerpFactor = config->GetLerpFactor();
 
@@ -36,8 +36,8 @@ namespace RAC
 		TEST_METHOD(ResetAbsorption)
 		{
 			const int delay = 73;
-			const Coefficients T60({ 0.1, 0.2, 0.3, 0.4 });
-			const Coefficients reflectionGains({ 0.87, 0.75, 0.81, 0.84 });
+			const Coefficients T60({ 0.1, 0.2, 0.3, 0.4, 0.4 });
+			const Coefficients reflectionGains({ 0.87, 0.75, 0.81, 0.84, 0.84 });
 			const std::shared_ptr<Config> config = std::make_shared<Config>();
 			const Real lerpFactor = config->GetLerpFactor();
 
@@ -59,8 +59,8 @@ namespace RAC
 		TEST_METHOD(ResetReflection)
 		{
 			const int delay = 73;
-			const Coefficients T60({ 0.1, 0.2, 0.3, 0.4 });
-			const Coefficients reflectionGains({ 0.87, 0.75, 0.81, 0.84 });
+			const Coefficients T60({ 0.1, 0.2, 0.3, 0.4, 0.4 });
+			const Coefficients reflectionGains({ 0.87, 0.75, 0.81, 0.84, 0.84 });
 			const std::shared_ptr<Config> config = std::make_shared<Config>();
 			const Real lerpFactor = config->GetLerpFactor();
 
@@ -84,9 +84,9 @@ namespace RAC
 		TEST_METHOD(ProcessReflection)
 		{
 			const int delay = 7;
-			const Coefficients T60({ 0.1, 0.2, 0.3, 0.4 });
+			const Coefficients T60({ 0.1, 0.2, 0.3, 0.4, 0.4 });
 			const Real target = 0.6;
-			const Coefficients reflectionGains({ target, target, target, target });
+			const Coefficients reflectionGains({ target, target, target, target, target });
 			const std::shared_ptr<Config> config = std::make_shared<Config>();
 			const Real lerpFactor = 1.0;
 
@@ -107,7 +107,7 @@ namespace RAC
 		TEST_METHOD(ProcessAbsorption)
 		{
 			const int delay = 750;
-			const Coefficients T60({ 1.2, 1.2, 1.2, 1.2 });
+			const Coefficients T60({ 1.2, 1.2, 1.2, 1.2, 1.2 });
 			const std::shared_ptr<Config> config = std::make_shared<Config>();
 			// config->lerpFactor = 0.5;
 			const Real lerpFactor = config->GetLerpFactor();
@@ -126,8 +126,8 @@ namespace RAC
 		TEST_METHOD(UpdateT60)
 		{
 			const int delay = 100;
-			const Coefficients T60({ 0.2, 0.2, 0.2, 0.2 });
-			const Coefficients newT60({ 0.6, 0.6, 0.6, 0.6 });
+			const Coefficients T60({ 0.2, 0.2, 0.2, 0.2, 0.2 });
+			const Coefficients newT60({ 0.6, 0.6, 0.6, 0.6, 0.6 });
 			const std::shared_ptr<Config> config = std::make_shared<Config>();
 			const Real lerpFactor = 1.0;
 
